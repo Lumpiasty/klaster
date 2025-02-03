fa65938836a7f56edabb878e57e7214f4e2091a6
Lumpiasty <arek.dzski@gmail.com>
2025-02-03 02:38:29 +0100

    add kubectl mayastor and openebs plugins

--- flake.nix
+++ flake.nix
@@ -4,14 +4,19 @@
 
         # Only to ease updating flake.lock, flake-compat is used by shell.nix
         flake-compat.url = https://flakehub.com/f/edolstra/flake-compat/1.1.0.tar.gz;
+
+        # Allows us to install krew plugins
+        krew2nix.url = "github:a1994sc/krew2nix";
+        krew2nix.inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    outputs = { self, nixpkgs, ... }: let 
+    outputs = { self, nixpkgs, krew2nix, ... }: let 
         system = "x86_64-linux";
     in {
         devShells."${system}".default =
         let
             pkgs = import nixpkgs {
+                overlays = [ krew2nix.overlay ];
                 inherit system;
             };
         in
@@ -19,7 +24,11 @@
                 packages = with pkgs; [
                     vim gnumake
                     talosctl cilium-cli
-                    kubectl kubectx k9s kubernetes-helm
+                    kubectx k9s kubernetes-helm
+                    (kubectl.withKrewPlugins (plugins: with plugins; [
+                        mayastor
+                        openebs
+                    ]))
                     ansible
                 ];
                 

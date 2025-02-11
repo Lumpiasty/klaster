4228c31a546f908699299f4732a273ce62027dfd
Lumpiasty <arek.dzski@gmail.com>
2025-02-11 23:25:16 +0100

    add flux cli to flake

--- flake.nix
+++ flake.nix
@@ -30,6 +30,7 @@
                         openebs
                     ]))
                     ansible
+                    fluxcd
                 ];
                 
                 shellHook = ''
